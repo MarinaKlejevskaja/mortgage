094dee032afc3e573c7b9ea0ccc03e15a3d19803
Marina Klejevskaja <marina.klejevskaja@gmail.com>
2023-04-13 21:59:11 +0300

    users table updated

--- src/main/java/com/academy/mortgage/users/Users.java
+++ src/main/java/com/academy/mortgage/users/Users.java
@@ -14,7 +14,7 @@ public class Users {
     @Id
     @GeneratedValue
     private Long id;
-    private  String firstName
-    private String lastName
+    private  String firstName;
+    private String lastName;
     private String email;
 }
