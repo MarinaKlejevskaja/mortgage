c836544ab5e426e6dc55bbc00e7b7cce9e144c4b
Marina Klejevskaja <37306041+MarinaKlejevskaja@users.noreply.github.com>
2023-04-13 23:01:23 +0300

    Delete src/main/java/com/academy/mortgage/user directory

--- src/main/java/com/academy/mortgage/user/UsersController.java
+++
@@ -1,9 +0,0 @@
-package com.academy.mortgage.user;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UsersController {
-    @Autowired
-    UsersRepository userRepository;
-}
--- src/main/java/com/academy/mortgage/user/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.academy.mortgage.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    List<Users> findAll();
-}
