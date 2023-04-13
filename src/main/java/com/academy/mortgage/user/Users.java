009516a958c2ba890b737ff765f8ff558f74366e
Marina Klejevskaja <37306041+MarinaKlejevskaja@users.noreply.github.com>
2023-04-13 22:03:47 +0300

    updated users entity

--- src/main/java/com/academy/mortgage/user/Users.java
+++ src/main/java/com/academy/mortgage/user/Users.java
@@ -15,8 +15,8 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private  String name;
-    private String surname;
+    private  String firstName;
+    private String lastName;
     private String email;
 
 }
