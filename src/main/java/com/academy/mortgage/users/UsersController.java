0845d82c2bae84ea4b19991acbfdd5fd95d95123
Marina Klejevskaja <37306041+MarinaKlejevskaja@users.noreply.github.com>
2023-04-13 22:51:23 +0300

    Update UsersController.java

--- src/main/java/com/academy/mortgage/users/UsersController.java
+++ src/main/java/com/academy/mortgage/users/UsersController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class UsersController {
     @Autowired
-    private UsersRepository usersRepository;
+    UsersRepository usersRepository;
 }
