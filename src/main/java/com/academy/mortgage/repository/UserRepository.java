e941a6b38c6bf7e46dcc37f3ec6b78ced5d9aa18
Ilze Rigerte <irigerte@gmail.com>
2023-04-13 14:40:58 +0300

    test db

---
+++ src/main/java/com/academy/mortgage/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.academy.mortgage.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRepository {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private  String name;
+    private String surname;
+    private String email;
+
+}
