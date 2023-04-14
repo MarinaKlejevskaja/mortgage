1c8dbd4a4e6e133360056d2f122f41a3224d6e4d
Marina Klejevskaja <marina.klejevskaja@gmail.com>
2023-04-14 13:04:58 +0300

    security config added

---
+++ src/main/java/com/academy/mortgage/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.academy.mortgage.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http
+                .httpBasic().disable()
+                .build();
+    }
+}
+
+
+
