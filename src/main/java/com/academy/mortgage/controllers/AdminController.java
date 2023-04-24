efe1c55b61fed3de8488e624f2cec1684659ec82
Ilze Rigerte <irigerte@gmail.com>
2023-04-24 13:13:25 +0300

    sql for admin

--- src/main/java/com/academy/mortgage/controllers/AdminController.java
+++ src/main/java/com/academy/mortgage/controllers/AdminController.java
@@ -5,6 +5,7 @@ import com.academy.mortgage.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class AdminController {
 
 
     @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> userList = userRepository.findAll();
         return new ResponseEntity<>(userList, HttpStatus.OK);
