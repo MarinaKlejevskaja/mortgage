7910d22feda096257cec4834934ca2f4801544be
Marina Klejevskaja <marina.klejevskaja@gmail.com>
2023-04-29 22:42:51 +0300

    return null if PN not found

--- src/main/java/com/academy/mortgage/services/UserService.java
+++ src/main/java/com/academy/mortgage/services/UserService.java
@@ -121,7 +121,13 @@ public class UserService {
     }
 
     public Long getPersonalNumber(String email) {
-       User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException(email));
-       return Long.parseLong(user.getPersonalNumber());
+       User user = userRepository.findByEmail(email).orElse(null);
+       if(user != null) {
+           String personalNumber = user.getPersonalNumber();
+           if(personalNumber != null) {
+               return Long.parseLong(personalNumber);
+           }
+       }
+       return null;
     }
 }
